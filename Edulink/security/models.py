6a854ff1d16c3e4a3d92161755e4ef4a50dcba80
Duncan <duncanmathai3@gmail.com>
2025-06-23 10:35:34 +0300

    recreate previously deleted app files, create comprehensive security monitoring system, made some changes for models.py

--- Edulink/security/models.py
+++ Edulink/security/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class LoginHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='login_histories')
     ip_address = models.GenericIPAddressField(null=True, blank=True)
+    user_agent = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
